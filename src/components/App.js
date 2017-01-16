1beee0ca59ad944e698e126d9853850cfa15a95a
DumeD <daviddume3@gmail.com>
2017-01-16 18:20:20 +0100

    updated board

--- src/components/App.js
+++ src/components/App.js
@@ -18,17 +18,47 @@ class App extends Component {
     };
 
     this.squareClick = this.squareClick.bind(this);
+    this.definePlayer = this.definePlayer.bind(this);
+  }
+
+  definePlayer() {
+    var currentPlayer = this.state.currentPlayer;
+    const O_PLAYER = this.state.O_PLAYER;
+    const X_PLAYER = this.state.X_PLAYER;
+    switch (currentPlayer) {
+      case '':
+        this.setState({ currentPlayer: X_PLAYER });
+        break;
+      case O_PLAYER:
+        this.setState({ currentPlayer: X_PLAYER });
+        break;
+      case X_PLAYER:
+        this.setState({ currentPlayer: O_PLAYER });
+        break;
+      default:
+        return null;
+    }
   }
 
   squareClick(index) {
-    console.log(index);
+    this.definePlayer();
+
+    const O_PLAYER = this.state.O_PLAYER;
+    const X_PLAYER = this.state.X_PLAYER;
+
+    if(this.state.board[index] === O_PLAYER || this.state.board[index] === X_PLAYER) {
+      return;
+    }
+
+    this.state.board.splice(index, 1, this.state.currentPlayer);
+
   }
 
   render() {
     return (
       <div className="App">
         {this.state.board.map((cell, index) => {
-          return <Square key={index} onClick={() => this.squareClick(index)} value={this.state.currentPlayer} />;
+          return <Square key={index} cellId={index} onClick={this.squareClick.bind(this, index)} value={cell} />;
         })}
       </div>
     );
--- src/components/square.js
+++ src/components/square.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class Square extends Component {
   render() {
     return (
-      <div className='square' onClick={this.props.onClick} value={this.props.value} />
+      <div className='square' onClick={this.props.onClick}>
+        {this.props.value}
+      </div>
     );
   }
 }
